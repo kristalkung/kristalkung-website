0e130ec0763162694098df8461cdbb0de7feb0e8
Kristal Kung <kristalkung@gmail.com>
2021-07-26 23:48:05 +0000

    added hamburger menu icon

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './header.css'
+import Mobile from './mobile'
+import Web from './web'
 
 function Header() {
     return (
-        <div>
-            This is header
+        <div className='header'>
+            <div className='logo'> Kristal Kung </div>
+            <div className='menu'></div>
+            <div className='web-menu'> <Web /> </div>
+            <div className='mobile-menu'> <a href='./public/menu.png'></a></div>
         </div>
     )
 }
