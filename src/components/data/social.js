0f2b75f2ad6870417bee5f48976245895dd6f71d
Kristal Kung <kristalkung@gmail.com>
2021-07-28 06:20:59 +0000

    social.js includes SocialData, an object that contains information for platform, link, and icon. This will be used for the contact portion

--- src/components/data/social.js
+++ src/components/data/social.js
@@ -2,16 +2,16 @@ export const SocialData = [
     {
         platform: "LinkedIn",
         link: "www.linkedin.com/in/kristalkung",
-        icon: require('../../../src/img/linkedin.png').default,
+        icon: require('../../../src/img/linkedin.png').default
     },
     {
         platform: "Github",
         link: "www.github.com/kristalkung",
-        icon: require('../../../src/img/github.png').default,
+        icon: require('../../../src/img/github.png').default
     },
     {
         platform: "Email",
         link: "kristalkung@gmail.com",
-        icon: require('../../../src/img/gmail.png').default,
+        icon: require('../../../src/img/gmail.png').default
     }
 ]
