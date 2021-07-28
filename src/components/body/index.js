4e2e22289e37b3e7e9f1d7b2c0ba93ec003537d0
Kristal Kung <kristalkung@gmail.com>
2021-07-28 00:51:31 +0000

    added body/index.js and body/body.css

--- src/components/body/index.js
+++ src/components/body/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import About from './about'
 import './body.css'
+import Contact from './contact'
+import Experience from './experience'
+import Projects from './projects'
+import Skills from './skills'
 
 function Body() {
     return (
-        <div className='body'></div>
+        <div className='body'>
+            <section id='about'>
+                <About />
+            </section>
+            <section id='projects'>
+                <Projects />
+            </section>
+            <section id='skills'>
+                <Skills />
+            </section>
+            <section id='experience'>
+                <Experience />
+            </section>
+            <section id='contact'>
+                <Contact />
+            </section>
+        </div>
     )
 }
 
