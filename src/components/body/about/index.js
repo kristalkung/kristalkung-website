e19056384f71984b4ed6bf169d26aca5915eb404
Kristal Kung <kristalkung@gmail.com>
2021-07-28 01:57:27 +0000

    changed pic color

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -14,6 +14,9 @@ function About() {
           <br /> Software engineer 💡 Home cook 🍔 
           <br /> Runner 🏃🏻‍♀️ Reader 📚 Ice cream enthusiast 🍦
         </div>
+        <div className='about-photo'>
+          <img className='picture' src={require('../../../static/img/about-pic.png').default} alt=''/>
+        </div>
       </div>
       <div className='about-bottom'> This is contacts buttons 
       </div>
