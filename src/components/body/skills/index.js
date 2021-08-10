04337f51693c6d0e9f0597089112f2897f98414d
Kristal Kung <kristalkung@gmail.com>
2021-08-10 00:24:23 +0000

    add skillsdata and skills

--- src/components/body/skills/index.js
+++ src/components/body/skills/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Separator from '../../common/separator';
+import './skills.css'
 
 function Skills() {
   return (
-    <div>
-      This is skills
+    <div className='skills'>
+      <Separator />
+      <label className='section-title'>Skills</label>
+      <div className=''></div>
     </div>
   )
 }
---
+++ src/components/data/skills.js
@@ -0,0 +1,73 @@
+export const SkillsData=[
+    {
+        type:"Frontend",
+        list:[
+            {
+                name:"React",
+                icon: <i class="devicon-react-original"></i>
+            },
+            {
+                name:"Javascript",
+                icon: <i class="devicon-javascript-plain colored"></i>
+            },
+            {
+                name:"JQuery",
+                icon: <i class="devicon-jquery-plain-wordmark colored"></i>
+            },
+            {
+                name:"Bootstrap",
+                icon: <i class="devicon-bootstrap-plain-wordmark colored"></i>
+            },
+            {
+                name:"CSS",
+                icon: <i class="devicon-css3-plain-wordmark colored"></i>
+            },
+            {
+                name:"HTML",
+                icon: <i class="devicon-html5-plain-wordmark colored"></i>
+            },
+        ]
+    },
+    {
+        type:"Backend",
+        list:[
+            {
+                name:"Python",
+                icon: <i class="devicon-python-plain-wordmark colored"></i>
+            },
+            {
+                name:"Flask",
+                icon: <i class="devicon-flask-original-wordmark colored"></i>
+            },
+            {
+                name:"PostgreSQL",
+                icon: <i class="devicon-postgresql-plain-wordmark colored"></i>
+            },
+            {
+                name:"SQLAlchemy",
+                icon: <i class="devicon-sqlalchemy-original-wordmark colored"></i>
+            },
+        ]
+    },
+    {
+        type:"Tools",
+        list:[
+            {
+                name:"VS Code",
+                icon: <i class="devicon-vscode-plain-wordmark colored"></i>
+            },
+            {
+                name:"Git",
+                icon: <i class="devicon-git-plain-wordmark colored"></i>
+            },
+            {
+                name:"Github",
+                icon: <i class="devicon-github-original-wordmark colored"></i>
+            },
+            {
+                name:"Vagrant",
+                icon: <i class="devicon-vagrant-plain-wordmark colored"></i>
+            },
+        ]
+    }
+]
