72b224d247d93b496a205493bbc5e95049c8794b
Kristal Kung <kristalkung@gmail.com>
2021-08-10 00:09:51 +0000

    edited about section for projects in projectdata

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -2,7 +2,7 @@ export const ProjectData = [
     {
         id: 1,
         title:"Recallama",
-        about:"Using the openFDA API, Recallama allows you to search for FDA drug and food recalls. Users may save recalls to their profile and share recalls via email",
+        about:"Recallama is my capstone project for Hackbright Academy's Software Engineering Fellowship. I created the back end using Python/Flask and simultaneously learned and implemented the front end in React. Using the openFDA API, Recallama allows you to search for FDA drug and food recalls. Users may save recalls to their profile and share recalls via email.",
         tags: ["Python", "Flask", "PostgreSQL", "React", "React Router", "CSS", "Bootstrap"],
         demo: "https://www.youtube.com/watch?v=_tk0AGjsjgM",
         github: "https://github.com/kristalkung/kristalkung-recallama",
@@ -12,7 +12,7 @@ export const ProjectData = [
     {
         id: 2,
         title:"Personal Portfolio",
-        about:"You're here! This is my second project in React and my first project using npm and NodeJS.",
+        about:"You're here! This is my second project in React and my first project using npm and NodeJS. For this project, I focused on styling and making the site compatible with viewing from different web devices.",
         tags: ["Javascript", "React", "HTML", "CSS"],
         demo: null,
         github: "https://github.com/kristalkung/kristalkung-website",
@@ -21,7 +21,7 @@ export const ProjectData = [
     {
         id: 3,
         title:"Movie Ratings App",
-        about:"A Flask application that allows users to view information on movies, login, and add personal ratings to a movie.",
+        about:"A Flask application that allows users to view information on movies, login, and add personal ratings to a movie. This is my first project utilizing a Python/Flask back end and creating a data model where there is a one to many relationship between users and movie ratings.",
         tags: ["Python", "Flask", "PostgreSQL"],
         demo: null,
         github: "https://github.com/kristalkung/movie-ratings-app",
