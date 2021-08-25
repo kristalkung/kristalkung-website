3f18795bb5e8e2e06ca14bba35da4f1c032b9b55
Kristal Kung <kristalkung@gmail.com>
2021-08-25 02:42:04 +0000

    edited label date font color and size for experience

--- src/components/body/experience/experience-card.js
+++ src/components/body/experience/experience-card.js
@@ -7,7 +7,7 @@ function ExperienceCard( {item}) {
             <img src={item.companyLogo} className='experience-logo' alt='logo'/>
             <div className='experience-info'>
                 <label className='company-name'>{item.company}</label>
-                <div className='work-dates'>
+                <div className='experience-dates'>
                     <label>{item.dateJoining} - </label> <label>{item.dateEnd}</label>
                 </div>
                 <div className='work-desc'>
