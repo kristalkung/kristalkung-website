99bc420b1d17291f35da50bf13f511c51753ae79
Kristal Kung <kristalkung@gmail.com>
2021-08-28 18:57:54 +0000

    removed banner

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -5,16 +5,15 @@ import Header from '../header'
 import './home.css'
 
 function Home() {
-    return (
-        <div>
-            <div> <Header /> </div>
-            <div className='home'>
-                <div> <Body /> </div>
-                <div> <Footer /> </div>
-            </div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <div className='home'>
+        <div> <Header /> </div>
+        <div> <Body /> </div>
+        <div> <Footer /> </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
